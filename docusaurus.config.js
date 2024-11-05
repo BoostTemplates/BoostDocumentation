f077063313f8456023bccf491b05495ee7f59ee7
PeterWaIIace <piotr.walas.prywatny@gmail.com>
2024-11-05 15:31:26 +0100

    updating website

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'BOOST_logo.png',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://boosttemplates.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/BoostDocumentation',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
